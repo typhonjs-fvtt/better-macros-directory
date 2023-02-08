d02b331f5dc95ed1ca6b7587749a84ea8db0520d
typhonrt <support@typhonjs.io>
2023-02-07 17:09:11 -0800

    Update for Vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,9 @@ import {
    postcssConfig,
    terserConfig }          from '@typhonjs-fvtt/runtime/rollup';
 
+// ATTENTION!
+// Please modify the below variables: s_PACKAGE_ID and s_SVELTE_HASH_ID appropriately.
+
 // For convenience, you just need to modify the package ID below as it is used to fill in default proxy settings for
 // the dev server.
 const s_PACKAGE_ID = 'modules/better-macros-directory';
@@ -14,17 +17,15 @@ const s_PACKAGE_ID = 'modules/better-macros-directory';
 // by shortening 'better-macros-directory'.
 const s_SVELTE_HASH_ID = 'bmd';
 
-const s_COMPRESS = true;  // Set to true to compress the module bundle.
+// Additional options
+const s_COMPRESS = false;  // Set to true to compress the module bundle.
 const s_SOURCEMAPS = true; // Generate sourcemaps for the bundle (recommended).
 
-// Used in bundling.
+// Used in bundling particularly during development. If you npm-link packages to your project add them here.
 const s_RESOLVE_CONFIG = {
    browser: true,
    dedupe: ['svelte', '@typhonjs-fvtt/runtime', '@typhonjs-fvtt/svelte-standard']
 };
-
-// ATTENTION!
-// Please modify the above variables: s_PACKAGE_ID and s_SVELTE_HASH_ID appropriately.
 
 export default () =>
 {
@@ -56,17 +57,17 @@ export default () =>
       // resources served with this particular Vite configuration. Modify the proxy rule as necessary for your
       // static resources / project.
       server: {
-         port: 30002,
-         open: false, //'/game',
+         port: 30001,
+         open: '/game',
          proxy: {
-            // Serves static files from main Foundry server
-            [`^(/${s_PACKAGE_ID}/(assets|lang|packs|style.css))`]: 'http://localhost:30001',
+            // Serves static files from main Foundry server.
+            [`^(/${s_PACKAGE_ID}/(assets|lang|packs|style.css))`]: 'http://localhost:30000',
 
             // All other paths besides package ID path are served from main Foundry server.
-            [`^(?!/${s_PACKAGE_ID}/)`]: 'http://localhost:30001',
+            [`^(?!/${s_PACKAGE_ID}/)`]: 'http://localhost:30000',
 
             // Enable socket.io from main Foundry server.
-            '/socket.io': { target: 'ws://localhost:30001', ws: true }
+            '/socket.io': { target: 'ws://localhost:30000', ws: true }
          },
          fs: { strict: false }
       },
@@ -88,10 +89,10 @@ export default () =>
       plugins: [
          svelte({
             compilerOptions: {
-               // Provides a custom hash adding `-bmd-` to scoped Svelte styles; "bmd" derived from
-               // 'better-macros-directory'. This is reasonable to do as the styles in TRL compiled across `n` different
-               // packages will be the same. Slightly modifying the hash ensures that your package has uniquely scoped
-               // styles for all TRL components.
+               // Provides a custom hash adding the string defined in `s_SVELTE_HASH_ID` to scope Svelte styles;
+               // This is reasonable to do as the framework styles in TRL compiled across `n` different packages will
+               // be the same. Slightly modifying the hash ensures that your package has uniquely scoped styles for all
+               // TRL components and makes it easier to review styles in the browser debugger.
                cssHash: ({ hash, css }) => `svelte-${s_SVELTE_HASH_ID}-${hash(css)}`
             },
             preprocess: preprocess()

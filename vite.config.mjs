63ea92e2021d19ac42a7fb9a3027cad7b35d9d48
typhonrt <support@typhonjs.io>
2023-07-03 17:03:57 -0700

    temp check in of Vite dev setup.

--- vite.config.mjs
+++ vite.config.mjs
@@ -24,7 +24,7 @@ const s_SOURCEMAPS = true; // Generate sourcemaps for the bundle (recommended).
 // Used in bundling particularly during development. If you npm-link packages to your project add them here.
 const s_RESOLVE_CONFIG = {
    browser: true,
-   dedupe: ['svelte']
+   dedupe: ['svelte', '@typhonjs-fvtt/runtime', '@typhonjs-fvtt/svelte-standard']
 };
 
 export default () =>
@@ -68,7 +68,8 @@ export default () =>
 
             // Enable socket.io from main Foundry server.
             '/socket.io': { target: 'ws://localhost:30000', ws: true }
-         }
+         },
+         fs: { strict: false }
       },
       build: {
          outDir: __dirname,

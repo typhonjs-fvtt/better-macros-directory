9017c326036ca74923e5eea9d4fc4e248157220f
typhonrt <support@typhonrt.org>
2023-09-04 12:36:57 +1200

    update for TRL 0.1.2

--- vite.config.mjs
+++ vite.config.mjs
@@ -24,7 +24,7 @@ const s_SOURCEMAPS = true; // Generate sourcemaps for the bundle (recommended).
 // Used in bundling particularly during development. If you npm-link packages to your project add them here.
 const s_RESOLVE_CONFIG = {
    browser: true,
-   dedupe: ['svelte', '@typhonjs-fvtt/runtime', '@typhonjs-fvtt/svelte-standard']
+   dedupe: ['svelte']
 };
 
 export default () =>
@@ -68,8 +68,7 @@ export default () =>
 
             // Enable socket.io from main Foundry server.
             '/socket.io': { target: 'ws://localhost:30000', ws: true }
-         },
-         fs: { strict: false }
+         }
       },
       build: {
          outDir: __dirname,
